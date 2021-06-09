9a0752e2d9d76a9b1fca8bf8c0b94dccb7ff7552
ssonko94 <ssonkojimmy@gmail.com>
2021-06-09 12:14:28 +0300

    import a Link component to implement spa routing

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/politics">The political News</Link>
+        </li>
+        <li>
+          <Link href="/news/finances">The Financial News</Link>
+        </li>
+        <li>
+          <Link href="/news/sports">The Sports News</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
