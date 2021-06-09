ead690980316d6204cbc800249d37e17aa0d0d69
ssonko94 <ssonkojimmy@gmail.com>
2021-06-09 18:31:41 +0300

    use the mongo db client object to load details of a single meetup

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,32 +1,43 @@
+import { MongoClient, ObjectId } from "mongodb";
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetailPage() {
+function MeetupDetailPage(props) {
   return (
     <MeetupDetail
-      image={`https://unsplash.com/photos/sI7IE_jEUWQ/download?force=true`}
-      title="The cosure Spot"
-      address="Nabweru 236, kibwa street Kampala"
-      description="The cosure Spot description"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://ssonko:4JmDQ7CyRijJSPC@cluster0.yhjz1.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupCollection = db.collection("meeetups");
+
+  const meetups = await meetupCollection
+    .find(
+      {},
+      {
+        _id: 1,
+      }
+    )
+    .toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
@@ -35,16 +46,30 @@ export async function getStaticProps(context) {
 
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(
+    "mongodb+srv://ssonko:4JmDQ7CyRijJSPC@cluster0.yhjz1.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupCollection = db.collection("meeetups");
+
+  const selectedMeetup = await meetupCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+
   console.log(meetupId);
 
   return {
     props: {
       meetupData: {
-        image: "https://unsplash.com/photos/sI7IE_jEUWQ/download?force=true",
-        id: meetupId,
-        title: "The cosure Spot",
-        address: "Nabweru 236, kibwa street Kampala",
-        description: "The cosure Spot description",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
