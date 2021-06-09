195280f2d9a9e11be64f8d9c43f17e501a5dbade
ssonko94 <ssonkojimmy@gmail.com>
2021-06-09 16:22:03 +0300

    render the new meetup form from which a new meetup will be created

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+
+function NewMeetupPage() {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  };
+  return (
+    
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+   
+  );
+}
+
+export default NewMeetupPage;
