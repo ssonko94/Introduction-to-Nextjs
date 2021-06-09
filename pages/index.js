abc518b037b2bcd0c5da2126cf7072219fbc3535
ssonko94 <ssonkojimmy@gmail.com>
2021-06-09 18:02:11 +0300

    fetch data from our own server side created nodejs using the client side

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
+import { MongoClient } from "mongodb";
+
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "The cosure Spot",
-    image: "https://unsplash.com/photos/sI7IE_jEUWQ/download?force=true",
-    address: "Nabweru 236, kibwa street Kampala",
-    description: "This is a place to have fun 😂",
-  },
-  {
-    id: "m2",
-    title: "New kabalagala",
-    image: "https://unsplash.com/photos/lg1XncqylVk/download?force=true",
-    address: "Kabalagala 127, main street Kyadondo",
-    description: "feel love like your in paradise",
-  },
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: "m1",
+//     title: "The cosure Spot",
+//     image: "https://unsplash.com/photos/sI7IE_jEUWQ/download?force=true",
+//     address: "Nabweru 236, kibwa street Kampala",
+//     description: "This is a place to have fun 😂",
+//   },
+//   {
+//     id: "m2",
+//     title: "New kabalagala",
+//     image: "https://unsplash.com/photos/lg1XncqylVk/download?force=true",
+//     address: "Kabalagala 127, main street Kyadondo",
+//     description: "feel love like your in paradise 😍",
+//   },
+// ];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -35,10 +37,26 @@ function HomePage(props) {
 // }
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://ssonko:4JmDQ7CyRijJSPC@cluster0.yhjz1.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meeetupCollection = db.collection("meeetups");
+
+  const meetups = await meeetupCollection.find().toArray();
+
+  client.close();
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
-      revalidate: 10
+      meetups: meetups.map(meeetup => ({
+        title: meeetup.title,
+        address: meeetup.address,
+        image: meeetup.image,
+        id: meeetup._id.toString(),
+      })),
+      revalidate: 10,
     },
   };
 }
